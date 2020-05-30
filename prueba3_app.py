e1e3746e125264ed143a0948515866ae722074e1
marmarquez <66099894+marmarquez@users.noreply.github.com>
2020-05-30 18:30:13 -0400

    version con pyserial
    Esta versión fue probada con puertos virtuales y funciona.
    Con puertos seriales

---
+++ prueba3_app.py
@@ -0,0 +1,107 @@
+from tkinter import *
+import serial
+import os
+os.system('clear')
+
+# creando ventana de GUI
+root = Tk()				
+root.title('LED ON-OFF with Python')
+root.geometry("550x250")
+
+flag = 1		# auxiliar para el boton
+
+puerto = serial.Serial() 		# definiendo objeto para la comunicacion
+puerto.baudrate = 9600
+puerto.timeout = 200
+
+# -----------------------------------------------------------------------------------
+# funcion que se ejecuta para enviar mensaje al mcu al presionar el boton2
+def mensajeLed():
+
+	global flag
+	global puerto 
+
+	if puerto.is_open == 1:  # si esta conectado
+		while 1==1:
+			if flag == 1:		# encender led
+				myButton2.config(bg='green')
+				myButton2.config(text='LED ON')
+				flag = 2
+				puerto.write(b'on')				# manda msj de encender
+				break
+
+			if flag == 2:		# apagar led
+				myButton2.config(bg='red')
+				myButton2.config(text='LED OFF')
+				flag = 1
+				puerto.write(b'off')		   # manda msj de apagar
+				break
+	else: # si no esta conectado
+		# abrir nueva ventana de dialogo
+		error2 = Tk()				
+		error2.title('errox02')
+		error2.geometry("250x100")
+
+		# por favor ingrese un puerto valido
+		myLabel5 = Label(error2,text="Debe conectarse a un puerto")
+		myLabel5.grid(padx=10,pady=20)
+
+# -------------------------------------------------------------------------------------
+# funcion que se ejecuta al presionar el boton1 para conectar con el puerto
+
+def conectar():
+
+	# si hay algo en el cuadro de texto, intento establecer conexion
+	if puerto.is_open == 0:			# si esta desconectado entro aqui
+		if portCom.get() != "":
+
+			puerto.port = portCom.get()
+
+			try:
+				puerto.open()
+				myLabel2.config(text="Conectado")
+				myButton1.config(text='Desconectar')
+			except:
+				puerto.close()
+				# no se logró conectar
+
+		# si la entrada de texto esta vacia	
+		if portCom.get() == "":
+
+			# abrir nueva ventana de dialogo
+			error = Tk()				
+			error.title('errox01')
+			error.geometry("200x100")
+
+			# por favor ingrese un puerto valido
+			myLabel4 = Label(error,text="Inserte puerto válido")
+			myLabel4.grid(padx=10,pady=20)
+	else:					# si esta conectado entro aqui
+		puerto.close()
+		myLabel2.config(text="Desonectado")
+		myButton1.config(text='Conectar')
+
+# ---------------------------------------------------------------------------
+# construccion de la ventana de GUI
+
+myLabel = Label(root,text="Establecer conexión con el puerto:")
+myLabel.grid(padx=10,pady=10)
+
+portCom = Entry(root,width=10)
+portCom.grid(padx=12,pady=12)
+
+portC = portCom.get()		# se obtiene el puerto al que se quiere conectar
+
+myButton1 = Button(root, text="Conectar",command=conectar)
+myButton1.grid(padx=12,pady=15)
+
+myLabel2 = Label(root,text="Desonectado")
+myLabel2.grid(padx=12,pady=20)
+
+myLabel3 = Label(root,text="Presione el boton para encender o apagar:")
+myLabel3.grid(row=1,column=2)
+
+myButton2 = Button(root, text="LED OFF",width=10,bg='red',command=mensajeLed)
+myButton2.grid(row=2,column=2)
+
+root.mainloop()
